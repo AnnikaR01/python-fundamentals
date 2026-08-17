f809e5aea5766ffdaa6fce647e89d52400bc5ace
Annika Rodgers <prod101453@gmail.com>
2026-08-17 07:41:41 -0700

    Add HIV facts to phase A exercise

--- phase_a_exercise.py
+++ phase_a_exercise.py
@@ -2,3 +2,7 @@ current_year=2026
 first_reported_year=1981
 years_since=current_year-first_reported_year
 print(f"Years since first reported {years_since}")
+fact_one = "HIV targets CD4 T cells."
+fact_two = "The CDC first reported HIV/AIDS cases in 1981."
+print(fact_one)
+print(fact_two)
