464b0f7aa16d8476138c19368c315a6a8b955e4b
Annika Rodgers <prod101453@gmail.com>
2026-08-17 19:13:06 -0700

    Replace placeholder numbers with sourced HIV data

---
+++ phase_b_exercise.py
@@ -0,0 +1,6 @@
+population = 60000000
+prevalence_rate = 16.6
+test_name = "HIV antibody test"
+print (f"Out of a population of {population}, roughly {prevalence_rate}% carry the {test_name} marker.")
+sensitivity_text = "99.94"
+sensitivity_number = float(sensitivity_text)
